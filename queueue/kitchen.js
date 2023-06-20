9592d87202a357b03b38cb536db988755ee050b4
FluttyProger <ktaraso@yandex.ru>
2023-06-20 08:30:56 +0300

    Update kitchen.js

--- queueue/kitchen.js
+++ queueue/kitchen.js
@@ -1,6 +1,5 @@
 const Queue = require('bee-queue');
 const axios = require('axios');
-const banana = require('@banana-dev/banana-dev');
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getStorage } = require('firebase-admin/storage');
 const { getDatabase } = require('firebase-admin/database');
@@ -33,24 +32,16 @@ try {
     var out = await axios.post(url, JSON.parse(job.data.jsn));
 
     const bucket = getStorage().bucket();
-    const ref = db.ref("Users/"+job.data.userid+"/Images/"+job.data.jobid);
-    const reff = db.ref("UserImages/"+job.data.userid+":"+job.data.jobid);
 //    console.log(out);
     const imageBuffer = Buffer.from(out.data.images[0], 'base64');
-    const jsondata = JSON.parse(out.data.info);
     const imageByteArray = new Uint8Array(imageBuffer);
     const file = bucket.file("images/"+job.data.userid+"/"+job.data.jobid+".png");
     const options = { resumable: false, metadata: { contentType: "image/png" } }
-    //console.log(jsondata);
-    const nsfww = jsondata.styles[0];
 
     await file.save(imageByteArray, options);
-    reff.update({nsfw:nsfww});
-    ref.update({Status: "done", nsfw:nsfww});
-//      console.log("eshkere");
-        done();
+    done();
 
 } catch(e) {
 
   console.log('Ошибка ' + e.name + ":" + e.message + "\n" + e.stack); // (3) <-
-});
+}});
